f537c98156ba18e74aacb88abc88e5cb5d030338
MadJangE00 <jiniixz@naver.com>
2021-09-15 15:58:52 +0900

    210915

--- chap08/double_list.py
+++ chap08/double_list.py
@@ -96,3 +96,40 @@ class DoubleLinkedList:
         """ 맨 뒤에 노드를 삽입 """
         self.current = self.head.prev
         self.add(data)
+
+    def remove_current_node(self) -> None:
+        """ 주목 노드 삭제 """
+        if not self.is_empty():
+            self.current.prev.next = self.current.next
+            self.current.next.prev = self.current.prev
+            self.current = self.current.prev
+            self.no -= 1
+            if self.current is self.head:
+                self.current = self.head.next
+        
+    def remove(self, p: Node) -> Node:
+        """ 노드 p를 삭제 """
+        ptr = self.head.next
+
+        while ptr is not self.head:
+            if ptr is p:
+                self.current = p
+                self.remove_current_node()
+                break
+            ptr = ptr.next
+
+    def remove_first(self) -> None:
+        """ 머리 노드 삭제 """
+        self.current = self.head.next
+        self.remove_current_node()
+
+    def remove_last(self) -> None:
+        """ 꼬리 노드 삭제 """
+        self.current = self.head.prev
+        self.remove_current_node()
+
+    def clear(self) -> None:
+        """ 모든 노드를 삭제 """
+        while not self.is_empty():
+            self.remove_first()
+        self.no = 0
